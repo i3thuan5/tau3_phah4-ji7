af0d5783507c17a297296067f3bf9f3d7a92d4c9
薛丞宏 <ihcaoe@gmail.com>
2016-08-10 15:44:40 +0800

    更新github網址

--- src/網站/頁尾.jsx
+++ src/網站/頁尾.jsx
@@ -8,16 +8,13 @@ export default class ToLam extends React.Component {
       <footer className='app footer inverted'>
         <ul className='ui menu container inverted'>
           <li className='item'>
-            <a href='https://g0v.hackpad.com/f4rSgcFTIzz'>我想幫忙</a>
-          </li>
-          <li className='item'>
             <a href='http://xn--jny.xn--v0qr21b.xn--kpry57d/'>相關專案</a>
           </li>
           <li className='item'>
             <a href='http://docs.tai5uan5gian5gi2hok8bu7.apiary.io/'>服務API</a>
           </li>
           <li className='item'>
-            <a href='https://github.com/sih4sing5hong5/sia2_siann2-mih4'>前端網站程式碼</a>
+            <a href='https://github.com/sih4sing5hong5/tau3_phah4-ji7'>前端網站程式碼</a>
           </li>
           <li className='item'>
             <a href='https://github.com/sih4sing5hong5/tai5-uan5_gian5-gi2_hok8-bu7/wiki'>後端服務程式碼</a>
