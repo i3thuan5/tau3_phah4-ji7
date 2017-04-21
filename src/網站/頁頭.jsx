a0cb66c0b1f269c711c8f5a1109754d60b3a5d53
a8568730 <a8568730@hotmail.com>
2017-04-21 17:33:00 +0800

    頁頭改用無內容的block

--- src/網站/頁頭.jsx
+++ src/網站/頁頭.jsx
@@ -1,17 +1,9 @@
 import React from 'react';
-import { Link } from 'react-router';
 
 export default class 頁頭 extends React.Component {
   render () {
     return (
-      <header className='app header'>
-        <div className='app bar container'>
-          <h1 className='title segment'>
-              
-          </h1>
-          <div></div>
-        </div>
-      </header>
+      <div className='app block'/>
     );
   }
 }
