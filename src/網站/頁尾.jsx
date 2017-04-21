f23d1d882b3d704d92d531a7637a3b098b8bcaee
a8568730 <a8568730@hotmail.com>
2017-04-21 15:44:23 +0800

    更新頁腳項目

--- src/網站/頁尾.jsx
+++ src/網站/頁尾.jsx
@@ -4,25 +4,31 @@ import { Link } from 'react-router';
 
 export default class ToLam extends React.Component {
   render () {
+    const sites = [{
+      title: 'Facebook',
+      url: 'https://www.facebook.com/i3thuan5//',
+    },
+    {
+      title: 'Github',
+      url: 'https://xn--v0qr21b.xn--kpry57d/',
+    },
+    {
+      title: '意傳文化科技',
+      url: 'https://xn--v0qr21b.xn--kpry57d/',
+    },
+    {
+      title: '語料來源',
+      url: 'https://github.com/sih4sing5hong5/tai5-uan5_gian5-gi2_hok8-bu7/wiki/Taiwanese-Corpus%E8%AA%9E%E6%96%99',
+    },];
+
+    const items = sites.map(item =>
+        <a className='item' href={item.url}>{item.title}</a>
+      );
     return (
       <footer className='app footer inverted'>
-        <ul className='ui menu container inverted'>
-          <li className='item'>
-            <a href='https://xn--v0qr21b.xn--kpry57d/'>意傳文化科技</a>
-          </li>
-          <li className='item'>
-            <a href='http://docs.tai5uan5gian5gi2hok8bu7.apiary.io/'>服務API</a>
-          </li>
-          <li className='item'>
-            <a href='https://github.com/sih4sing5hong5/tau3_phah4-ji7'>前端網站程式碼</a>
-          </li>
-          <li className='item'>
-            <a href='https://github.com/sih4sing5hong5/tai5-uan5_gian5-gi2_hok8-bu7/wiki'>後端服務程式碼</a>
-          </li>
-          <li className='item'>
-            <a href='https://github.com/sih4sing5hong5/tai5-uan5_gian5-gi2_hok8-bu7/wiki/Taiwanese-Corpus%E8%AA%9E%E6%96%99'>語料來源</a>
-          </li>
-        </ul>
+        <div className='ui stackable menu container inverted'>
+          {items}
+        </div>
       </footer>
     );
   }
