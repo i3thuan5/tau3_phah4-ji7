d8a0e74bcb9b29c5b34a3173b5396ff05b09d303
a8568730 <a8568730@hotmail.com>
2017-04-25 19:02:55 +0800

    用demo-ui的MainSection和Footer 移除html的semantic-ui外部連結

--- src/頁/查/查.jsx
+++ src/頁/查/查.jsx
@@ -3,22 +3,17 @@ import { browserHistory } from 'react-router';
 import Debug from 'debug';
 import Container翻譯結果 from '../../元素/翻譯/翻譯結果.container';
 import Container查表格 from './查表格.container';
+import { MainSection } from 'demo-ui';
 
 var debug = Debug('tau3:查');
 
 export default class 查 extends React.Component {
   render () {
     return (
-      <div className='app main block'>
-        <div className='ui grid text container'>
-        <div className='sixteen column'>
-        
-          <Container查表格 語句={this.props.語句}/>
-          <Container翻譯結果/>
-        
-        </div>
-        </div>
-      </div>
+    <MainSection>
+        <Container查表格 語句={this.props.語句}/>
+        <Container翻譯結果/>
+     </MainSection>
     );
   }
 }
