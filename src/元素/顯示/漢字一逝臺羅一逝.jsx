3c6d905081013913911c0198c4e5d2e57bb54eb3
a8568730 <a8568730@hotmail.com>
2017-05-02 16:42:31 +0800

    套用漢羅詞

--- src/元素/顯示/漢字一逝臺羅一逝.jsx
+++ src/元素/顯示/漢字一逝臺羅一逝.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import PropTypes from "prop-types";
 import 合成結果 from "../合成/合成結果";
+import 漢羅詞 from "./漢羅詞";
 import "./漢字一逝臺羅一逝.css";
 
 class 漢字一逝臺羅一逝 extends React.Component {
@@ -9,25 +10,21 @@ class 漢字一逝臺羅一逝 extends React.Component {
     const { 腔口, 查詢結果 } = this.props;
 
     const 綜合標音 = 查詢結果.綜合標音.map(
-      (綜音, i) => (
-        <table key={i} className='app table'>
-          <tbody>
-            <tr>
-              <td>
-                <合成結果
-                  腔口={腔口}
-                  語句={綜音.分詞}/>
-              </td>
-              <td>{綜音.漢字}</td>
-            </tr>
-            <tr>
-              <td/>
-              <td>{綜音.臺羅閏號調}</td>
-            </tr>
-          </tbody>
-        </table>
-      ),
-    );
+      (綜音, i) => {
+        const 漢字陣列 = 綜音.漢字.split(" ");
+        const 臺羅陣列 = 綜音.臺羅閏號調.split(" ");
+        const 標音陣列 = 漢字陣列.map((漢字, k) => (
+            <漢羅詞 key={k}
+            漢字詞={漢字}
+            臺羅閏號調={臺羅陣列[k]} />
+        ));
+        return (
+        <div key={i} className='app block'>
+          <合成結果 腔口={腔口} 語句={綜音.分詞}/>
+          {標音陣列}
+        </div>
+        );
+      });
     return (
         <div>
           {綜合標音}
