2200711114b34ccea2d315198065e1c7715a51b6
a8568730 <a8568730@hotmail.com>
2017-06-12 12:42:52 +0800

    套用ten1su4@1.0.0。在webpack載入taupahji.config.js

---
+++ taupahji.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  專案: "鬥拍字",
+  腔口: ["閩南語"],
+  範例查詢: "大家好",
+  頁尾連結: [{
+    title: "Facebook",
+    url: "https://www.facebook.com/i3thuan5/",
+  },
+  {
+    title: "Github",
+    url: "https://github.com/i3thuan5/tau3_phah4-ji7",
+  },
+  {
+    title: "意傳文化科技",
+    url: "https://xn--v0qr21b.xn--kpry57d/",
+  },
+  {
+    title: "語料來源",
+    url: "https://github.com/sih4sing5hong5/tai5-uan5_gian5-gi2_hok8-bu7/wiki/Taiwanese-Corpus%E8%AA%9E%E6%96%99",
+  }],
+};
